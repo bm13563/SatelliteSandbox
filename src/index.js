edaffc3a7633ac36e35bb5c7f9fe268d199b2fcd
bm13563 <bm13563@my.bristol.ac.uk>
2020-08-02 16:09:05 +0100

    switched to openlayers, rendering native canvas to new canvas

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
-import { Map } from "./modules/Map.js";
-import { Layer } from "./modules/Layer.js";
-import { Environment } from "./modules/Environment.js";
-import * as utils from "./modules/Utils.js"
-import "../node_modules/leaflet/dist/leaflet.css";
+import "../node_modules/ol/ol.css";
 import "./styles/page.css";
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
+import * as util from './modules/utils.js'
 
-var environment = new Environment();
-var testLayer = new Layer({ 
-    url: environment.variables.testTile,
-    options: {
-        layers: "NDVI",
-        format: "image/png",
-        showLogo: false,
-    }
- });
-var testMap = new Map()
-testMap.addLayer(testLayer);
+const testMapView = new View({
+    center: [-7337.954715, 6709336.594760],
+    zoom: 7,
+})
 
-testLayer.layer.on('load', function() { utils.tileMapToCanvasMap(); });
+const testWMS = new TileWMS({
+    url: "https://services.sentinel-hub.com/ogc/wms/061d4336-8ed1-44a5-811e-65eea5ee06c4",
+    params: {
+        'LAYERS': "NDVI", 
+        'TILED': true, 
+        'FORMAT': 'image/png',
+        'showLogo': false,
+        'CRS': "EPSG:3857",
+    },
+    attribution: "test",
+    crossOrigin: "anonymous",
+});
 
+const testMapLayer = new TileLayer({
+    source: testWMS,
+    visible: true,
+    title: "Sentinel testing",
+    opacity: 1,
+    minZoom: 6,
+});
 
+const testMap = new Map({
+  target: "tile_map",
+  layers: [testMapLayer],
+  view: testMapView,
+});
+
+testMap.on("postrender", () => {
+    var imageData = util.canvasToImageData("#tile_map canvas");
+    util.imageDataToCanvas("#canvas_map", imageData);
+})
--- src/modules/Utils.js
+++ src/modules/Utils.js
@@ -1,58 +1,20 @@
-export const initialise = () => {
-    const canvas = document.createElement("canvas")
-    const gl = canvas.getContext("webgl2");
-    canvas.id = 'objectGlCanvas'
-    document.body.appendChild(canvas);
-    if (gl === null) {
-        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-        return;
-    }
-    return gl;
+export const initialMapLoad = (map, callback, ...params) => {
+    map.once('postrender', () => {
+        callback(params)
+    });
 }
 
-export const randomInt = (maxVal) => {
-    return Math.floor(Math.random() * maxVal);
-  }
-
-export const createRandomF32Array = (arrSize, maxVal) => {
-    return Float32Array.from({length: arrSize}, () => randomInt(maxVal));
+export const canvasToImageData = (selector) => {
+    const canvas = document.querySelector(selector);
+    const context = canvas.getContext('2d');
+    return context.getImageData(0, 0, canvas.width, canvas.height);
 }
 
-export const generateQuad = (x, y, width, height) => {
-    var x1 = x;
-    var x2 = x + width;
-    var y1 = y;
-    var y2 = y + height;
-    return new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2,])
-}
 
-export const toRadians = (angle) => {
-    return angle /180 * Math.PI;
-}
-
-export const tileMapToCanvasMap = () => {
-    const canvas = document.getElementById("canvas_map");
+export const imageDataToCanvas = (selector, data) => {
+    const canvas = document.querySelector(selector);
+    const context = canvas.getContext('2d');
     canvas.width = canvas.getBoundingClientRect().width;
     canvas.height = canvas.getBoundingClientRect().height;
-    const context = canvas.getContext('2d');
-    const mapTiles = document.getElementsByClassName("leaflet-tile-loaded");
-    const paneContainer = document.getElementsByClassName("leaflet-map-pane")[0];
-    const tileContainer = mapTiles[0].parentElement;
-    const zoomTransform = parseTransform(paneContainer);
-    const panTransform = parseTransform(tileContainer);
-    const tileTransform = [zoomTransform[0] + panTransform[0], zoomTransform[1] + panTransform[1]];
-    for (let x = 0; x < mapTiles.length; x++) {
-        var imageWidth = mapTiles[x].naturalWidth;
-        var imageHeight = mapTiles[x].naturalHeight;
-        var imageTransform = parseTransform(mapTiles[x]);
-        context.drawImage(mapTiles[x], 0, 0, imageWidth, imageHeight, imageTransform[0] + tileTransform[0], imageTransform[1] + tileTransform[1], imageWidth, imageHeight);
-    }
-}
-
-export const parseTransform = (element) => {
-    const transformMatrix = element.style.transform;
-    var regExp = /\(([^)]+)\)/;
-    var parsedTransformMatrix = regExp.exec(transformMatrix)[1];
-    parsedTransformMatrix = parsedTransformMatrix.replace(/[^0-9\-.,]/g, "").split(",");
-    return [parseInt(parsedTransformMatrix[0]), parseInt(parsedTransformMatrix[1])]
+    context.putImageData(data, 0, 0);
 }
