5522629e43841b6cff045cf5cddba52f74384aeb
bm13563 <bm13563@my.bristol.ac.uk>
2020-09-05 00:51:30 +0100

    added in custom tile caching system
    openlayers tile caching system doesnt always prevent excess tile requests. custom caching system does

--- src/modules/layer.js
+++ src/modules/layer.js
@@ -4,7 +4,7 @@ import {Map, View} from 'ol';
 // canvas for opengl to read from. this class creates a "layer" with it's own canvas from
 // an openlayers layer. layers will be synced as long as the same view is used
 export class LayerObject{
-    constructor(olLayer, olView, bufferValue) {
+    constructor(olLayer, olView, bufferValue, cacheSize) {
         this.type = 'layerObject';
         this.olLayer = olLayer;
         this.olView = olView;
@@ -15,15 +15,21 @@ export class LayerObject{
         this.olMap;
         this.shaders = {};
         this.activeShader;
+        // this is kinda ridiculous and definitely needs refactoring
+        this._cacheSize = 250;
+        this._imageCache = {};
+        this._cacheOrder = {};
+        this._cacheIndex = 0;
+        this._cacheDelete = 0;
         this._createCanvasElement();
         this._createMap();
+        this._preventCachedTilesBeingRequested();
     }
 
     // TODO tidy up this function -> work out if we give a shit about ol controls - it's easy enough to control a map without right?
     _createCanvasElement = () => {
         // get the container for the tile elements, pass the size of the container to apply buffer to the canvas container
         const container = document.getElementById("tile_container");
-        const openlayerControls = document.getElementById("")
         const boundingRect = container.getBoundingClientRect();
         const div = document.createElement("div");
         div.classList.add("layer_object");
@@ -33,6 +39,7 @@ export class LayerObject{
         div.height = this.bufferValue * boundingRect.height;
         div.style.width = `${div.width}px`;
         div.style.height = `${div.height}px`;
+        // need to buffer the layer to ensure it's centred on our visible canvas. allows us to still use ol controls properly
         div.style.marginLeft = `-${((this.bufferValue * boundingRect.width) - boundingRect.width) / 2}px`
         div.style.marginTop = `-${((this.bufferValue * boundingRect.height) - boundingRect.height) / 2}px`;
         div.style.position = "absolute";
@@ -49,7 +56,7 @@ export class LayerObject{
             layers: [this.olLayer],
             view: this.olView,
         });
-        map.getView().setZoom(12);
+        map.getView().setZoom(15);
         // render the map without animation - prevents artifacts and reduces gpu overhead
         this.olLayer.getSource().tileOptions.transition = 0;
         // layers are not requested until they are used -> saves requests
@@ -57,7 +64,31 @@ export class LayerObject{
         this.olMap = map;
     }
 
-    // _preventCachedTilesBeingRequested = () => {
+    // prevent any layer from requesting tiles that have been cached. saves requests
+    _preventCachedTilesBeingRequested = () => {
+        this.olLayer.getSource().setTileLoadFunction((imageTile, src) => {
+            // reformat the current tile source to match the key of the tile cache
+            let tileCoords = imageTile.getTileCoord().join("/");
+            // check if the tile has been cached
+            if (tileCoords in this._imageCache) {
+                console.log("using cached image")
+                imageTile.image_ = this._imageCache[tileCoords];
+            } else {
+                imageTile.getImage().src = src;
+                this._addToImageCache(tileCoords, imageTile.image_);
+            }
+        })
+    }
 
-    // }
+    // makes sure that cache size doesn't exceed the cache limit - deletes the oldest tile if necessary
+    _addToImageCache = (key, image) => {
+        let imagesInCache = Object.keys(this._imageCache).length;
+        if (imagesInCache >= this._cacheSize) {
+            delete this._imageCache[this._cacheOrder[this._cacheDelete]];
+            this._cacheDelete++;
+        }
+        this._imageCache[key] = image;
+        this._cacheOrder[this._cacheIndex] = key;
+        this._cacheIndex++;
+    }
 }
